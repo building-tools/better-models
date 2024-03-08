e6000b3a54bed7332d399ff4b408eafecd1e50b4
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-08 17:51:08 +0100

    initial commit

---
+++ src/main/java/de/raphaelgoetz/bettermodels/BetterModels.kt
@@ -0,0 +1,13 @@
+package de.raphaelgoetz.bettermodels
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BetterModels : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
