b75f1e9f44377dbcae70e0d3faf49743db0eada1
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-09 12:59:56 +0100

    feat: added command to open the menu of all available models

--- src/main/java/de/raphaelgoetz/bettermodels/commands/GetModel.kt
+++ src/main/java/de/raphaelgoetz/bettermodels/commands/GetModel.kt
@@ -1,5 +1,19 @@
 package de.raphaelgoetz.bettermodels.commands
 
-class GetModel {
-    //TODO: COMMAND TO GET NEAREST MODEL AND PUT IT INTO THE INVENTORY OF THE PLAYER
+import de.raphaelgoetz.bettermodels.BetterModels
+import de.raphaelgoetz.bettermodels.menu.ModelMenu
+import net.axay.kspigot.gui.openGUI
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class GetModel(private val betterModels: BetterModels) : CommandExecutor {
+
+    override fun onCommand(player: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
+        if (player !is Player) return true
+        val menu = ModelMenu(betterModels).menu
+        player.openGUI(menu)
+        return false
+    }
 }
