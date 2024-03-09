41c2252e532d0608bc527ba8b98c9df668a9d74f
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-09 13:03:52 +0100

    feat: added command to remove nearest model

--- src/main/java/de/raphaelgoetz/bettermodels/commands/RemoveModel.kt
+++ src/main/java/de/raphaelgoetz/bettermodels/commands/RemoveModel.kt
@@ -1,5 +1,23 @@
 package de.raphaelgoetz.bettermodels.commands
 
-class RemoveModel {
-    //TODO: REMOVE NEAREST MODEL OF PLAYER
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+
+class RemoveModel : CommandExecutor {
+
+    override fun onCommand(player: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
+        if (player !is Player) return true
+
+        val entities = player.getNearbyEntities(0.5, 0.0, 0.5)
+        for (entity in entities) {
+            if (entity.type != EntityType.ITEM_DISPLAY) continue
+            entity.remove()
+        }
+
+        player.sendActionBar("§cRemoved item-display")
+        return false
+    }
 }
