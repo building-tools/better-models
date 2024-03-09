e0700781e9aedc246ba553eb6d6b94e8d93c4edb
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-09 12:59:31 +0100

    feat: added command to enter the edit-model mode

--- src/main/java/de/raphaelgoetz/bettermodels/commands/EditModel.kt
+++ src/main/java/de/raphaelgoetz/bettermodels/commands/EditModel.kt
@@ -1,5 +1,16 @@
 package de.raphaelgoetz.bettermodels.commands
 
-class EditModel {
-    //TODO COMMAND TO PUT PLAYER INTO EDIT MODE
+import de.raphaelgoetz.bettermodels.BetterModels
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class EditModel(private val betterModels: BetterModels) : CommandExecutor {
+
+    override fun onCommand(player: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
+        if (player !is Player) return true
+        betterModels.playerManager.toggleEditorInventory(player)
+        return false
+    }
 }
