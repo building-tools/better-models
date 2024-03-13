c0e688869db508ec8f166b18907bf07648889c8f
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-13 14:44:17 +0100

    feat: added listener to place model

--- src/main/java/de/raphaelgoetz/bettermodels/listeners/PlayerPlaceCustomModelListener.kt
+++ src/main/java/de/raphaelgoetz/bettermodels/listeners/PlayerPlaceCustomModelListener.kt
@@ -1,14 +1,49 @@
 package de.raphaelgoetz.bettermodels.listeners
 
-import de.raphaelgoetz.bettermodels.BetterModels
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.BlockFace
+import org.bukkit.block.data.type.Slab
+import org.bukkit.entity.ItemDisplay
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.EquipmentSlot
 
-class PlayerPlaceCustomModelListener(private val betterModels: BetterModels) : Listener {
+class PlayerPlaceCustomModelListener : Listener {
 
     @EventHandler
     fun onPlayerInteractEvent(playerInteractEvent: PlayerInteractEvent) {
-        //TODO: PLACE MODEL ON SOLID BLOCK OR SLAB
+
+        if (playerInteractEvent.hand == EquipmentSlot.OFF_HAND) return
+        if (!playerInteractEvent.action.isRightClick) return
+        if (playerInteractEvent.clickedBlock == null) return
+
+        val item = playerInteractEvent.item ?: return
+        if (item.type != Material.PAPER) return
+        if (item.itemMeta.customModelData == 0) return
+
+        val interactLocation = playerInteractEvent.interactionPoint ?: return
+        val y = if (interactLocation.block.blockData is Slab) interactLocation.blockY + 0.5 else interactLocation.blockY
+        val location = Location(
+            interactLocation.world,
+            interactLocation.blockX + 0.5,
+            y.toDouble() + 0.5,
+            interactLocation.blockZ + 0.5
+        )
+
+        val itemDisplay: ItemDisplay = location.world.spawn(location, ItemDisplay::class.java)
+        itemDisplay.setRotation(getRotation(playerInteractEvent.player), 0.0f)
+        itemDisplay.itemStack = item
+    }
+
+    private fun getRotation(player: Player): Float {
+        return when (player.facing) {
+            BlockFace.NORTH -> 180.0f
+            BlockFace.EAST -> -90.0f
+            BlockFace.WEST -> 90.0f
+            else -> 0.0f
+        }
     }
 }
